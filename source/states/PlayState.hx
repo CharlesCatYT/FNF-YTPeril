d6ccf403eb044d204db14d7dc6d1d5873bef9bdb
BeastlyGabi <45212377+BeastlyGhost@users.noreply.github.com>
2023-07-31 03:14:34 -0400

    oops

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2586,7 +2586,7 @@ class PlayState extends MusicBeatState
 	}
 
 	public var strumsBlocked:Array<Bool> = [];
-	private inline function onKeyPress(event:KeyboardEvent):Void
+	private function onKeyPress(event:KeyboardEvent):Void
 	{
 		var eventKey:FlxKey = event.keyCode;
 		var key:Int = getKeyFromEvent(keysArray, eventKey);
@@ -2624,7 +2624,7 @@ class PlayState extends MusicBeatState
 						for (bad in 1...sortedNotesList.length)
 						{
 							var doubleNote:Note = sortedNotesList[bad];
-							// no point in jack derection if it isn't a jack
+							// no point in jack detection if it isn't a jack
 							if (doubleNote.noteData != epicNote.noteData)
 								break;
 	
@@ -2680,7 +2680,7 @@ class PlayState extends MusicBeatState
 		return FlxSort.byValues(FlxSort.ASCENDING, a.strumTime, b.strumTime);
 	}
 
-	private inline function onKeyRelease(event:KeyboardEvent):Void
+	private function onKeyRelease(event:KeyboardEvent):Void
 	{
 		var eventKey:FlxKey = event.keyCode;
 		var key:Int = getKeyFromEvent(keysArray, eventKey);
