4b0a058a3b317120d2b95b00d417b7ada42909f6
CharlesCatYT <sptubea@gmail.com>
2023-08-02 23:43:57 -0400

    whoops!

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -106,7 +106,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "YouTube Animation Peril v" + ytPerilVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "YouTube Animation Peril v" + ytPerilVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
@@ -175,7 +175,7 @@ class MainMenuState extends MusicBeatState
 			{
 				if (optionShit[curSelected] == 'discord')
 				{
-					CoolUtil.browserLoad('https://ninja-muffin24.itch.io/funkin');
+					CoolUtil.browserLoad('https://discord.gg/WRm7Xkp75F');
 				}
 				else
 				{
