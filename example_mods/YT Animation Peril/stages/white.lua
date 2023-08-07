19f428db70dc5af7c90f9724a500d9cd178828f4
CharlesCatYT <sptubea@gmail.com>
2023-08-07 13:47:14 -0400

    grr

--- example_mods/YT Animation Peril/stages/white.lua
+++ example_mods/YT Animation Peril/stages/white.lua
@@ -1,6 +1,7 @@
 function onCreate()
 	-- background shit
-makeGraphic('whitey', 1280, 1920, '000000')
+    makeLuaSprite('whitey', 'empty', -1000, -125)
+makeGraphic('whitey', 5000, 5000, 'FFFFFF')
         addLuaSprite('whitey', false);
 
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
