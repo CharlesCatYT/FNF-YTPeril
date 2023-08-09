6b475a627ab751f4af06bdd2ff02f60f8afab64e
CharlesCatYT <sptubea@gmail.com>
2023-08-09 19:57:14 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -7,6 +7,7 @@ import openfl.Assets;
 import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
+import openfl.display.Bitmap;
 import flixel.math.FlxMath;
 import flixel.util.FlxColor;
 import openfl.events.Event;
