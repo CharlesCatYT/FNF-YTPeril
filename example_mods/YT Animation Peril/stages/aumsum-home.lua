9772a8a05a488d27597a9cbfd905480998edc546
Isaiah Mods (Official) <118643764+IsaiahModsOfficial@users.noreply.github.com>
2023-08-17 19:31:19 -0700

    fixed

--- example_mods/YT Animation Peril/stages/aumsum-home.lua
+++ example_mods/YT Animation Peril/stages/aumsum-home.lua
@@ -1,12 +1,12 @@
 function onCreate()
 
-	makeLuaSprite('skyaum','aumsum_home_sky',-95,-60)
+	makeLuaSprite('skyaum','aumsum_home_sky',-550,-200)
 	addLuaSprite('skyaum',false)
 
-	makeLuaSprite('houseaum','aumsum_home_house',-95,-60)
+	makeLuaSprite('houseaum','aumsum_home_house',-550,-200)
 	addLuaSprite('houseaum',false)
 
-	makeLuaSprite('signaum','aumsum_home_sign',1555,600)
+	makeLuaSprite('signaum','aumsum_home_sign',1700,750)
 	addLuaSprite('signaum',false)
 
 	close(true)
